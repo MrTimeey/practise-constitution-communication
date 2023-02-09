dd2673f8ec7c5e6ecd4a4810ccccd7545fcfdde5
Tim Siegler <Tim.Siegler@fincon.eu>
2023-02-09 16:44:10 +0100

    Redesign

--- src/views/CoachView.vue
+++ src/views/CoachView.vue
@@ -6,7 +6,7 @@ const userStore = storeToRefs(useUserStore());
 </script>
 
 <template>
-    <div class="p-4 min-h-screen flex flex-col text-gray-300">
+    <div class="p-4 flex flex-col text-gray-300">
         <h1 class="mb-8 text-3xl">Hallo {{ userStore.username.value }}!</h1>
         <h3 class="py-4 text-l">Aktuelle Daten:</h3>
         <p>Text</p>
--- src/views/PlayerView.vue
+++ src/views/PlayerView.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
 import { useUserStore } from '@/stores/user';
 import { storeToRefs } from 'pinia';
+import StarRating from '@/components/StarRating.vue';
 
 const userStore = storeToRefs(useUserStore());
 </script>
 
 <template>
     <div class="p-4 flex flex-col text-gray-300">
-        <h1 class="mb-8 text-3xl">Hallo {{ userStore.username.value }}!</h1>
-        <h3 class="py-4 text-l">Deine nächsten Termine:</h3>
-        <p>Text</p>
+        <p class="text-3xl mt-5 font-extrabold">Event-Name</p>
+        <p class="mb-12 text-l">am 01.01.2024</p>
+        <p class="mb-4 text-l">Wie ist deine körperliche Verfassung?</p>
+        <star-rating class="mb-8" />
+        <p class="mb-4 text-l">Wie ist deine geistige Verfassung?</p>
+        <star-rating class="mb-8" />
+        <p class="text-l mb-4">Anmerkungen</p>
+        <textarea class="textarea textarea-bordered" placeholder="Anmerkungen"></textarea>
+        <div class="card-actions justify-end mt-8">
+            <button class="btn btn-secondary">Abschicken</button>
+        </div>
     </div>
 </template>
